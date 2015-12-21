f76dc18eb88b54f4debab0ebfeb184a04e379cbf
Joey Cheng <joeycheng@joeys-macbook-pro.local>
2015-12-21 14:40:32 +0800

    Initial Commit

---
+++ singalViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  singalViewTest
+//
+//  Created by Joey Cheng on 21/12/2015.
+//  Copyright © 2015 FYP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
